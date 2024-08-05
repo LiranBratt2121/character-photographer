47329be2dd5812b2e7de6153b6dcc3eb28cbe2a8
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-06 02:12:16 +0300

    Utilize hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
-import React, { useState } from "react";
-import Camera from "./Camera";
-import HebrewCharacterSelector from "./HebrewCharacterSelector";
-import ResultScreen from "./ResultScreen";
+import React, { useState } from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import AppContent from './hooks/AppContent';
+
 
 const App: React.FC = () => {
-  const [selectedCharacter, setSelectedCharacter] = useState<string>("");
-  const [isCameraOpen, setIsCameraOpen] = useState<boolean>(false);
-  const [img, setImg] = useState<string>("");
-
-  const handleCharacterSelect = (character: string) => {
-    setSelectedCharacter(character);
-  };
-
-  const handleCameraOpen = (open: boolean) => {
-    setIsCameraOpen(open);
-  };
-
-  const handleOnImageCapture = (imageString: string) => {
-    setImg(imageString);
-    setIsCameraOpen(false);
-  };
-
-  const handleBackToSelector = () => {
-    setImg("");
-    setSelectedCharacter("");
-  };
-
   return (
-    <div>
-      {img && selectedCharacter ? (
-        <ResultScreen 
-          image={img} 
-          character={selectedCharacter} 
-          onBack={handleBackToSelector} 
-        />
-      ) : isCameraOpen ? (
-        <Camera OnImageCapture={handleOnImageCapture} />
-      ) : (
-        <HebrewCharacterSelector 
-          onCharacterSelect={handleCharacterSelect} 
-          onOpenCamera={handleCameraOpen} 
-        />
-      )}
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   );
 };
 
