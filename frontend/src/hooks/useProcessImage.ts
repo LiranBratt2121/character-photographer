9db8068146e4bc7de333fd7b6eab50cd6c7b615c
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-06 12:01:27 +0300

    Add useProcessImage hook

---
+++ frontend/src/hooks/useProcessImage.ts
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import {useState} from 'react'
+
+export interface ProcessImageProps {
+    imageString64: string;
+    true_character: string;
+    host: string
+    port: string
+    endPoint: string
+    args?: string
+}
+
+export type ProcessedImageResponse = {
+    result? : {
+        detected_character: string;
+        true_character: string;
+        og_image: string;
+        processed_image: string;
+        server_time_stamp: string;
+    };
+    error?: string
+}
+
+const useProcessImage = ({imageString64, true_character, host, port, endPoint, args}: ProcessImageProps) => {
+  const [data, setData] = useState<ProcessedImageResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const processImage = async () => {
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append('image_string', imageString64);
+    formData.append('true_character', true_character);
+
+    const path = `http://${host}:${port}/${endPoint}`
+    console.log(path)
+    try {
+        const response = await axios.post(
+            path,
+            formData,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+        )
+
+        setData(response.data);
+    }catch (error) {
+        if (error instanceof Error){
+            setError(error.message);
+        }else {
+            setError('An unknown error occurred');
+        }
+
+    }finally {
+        setLoading(false);
+    }
+  }
+  return {data, loading, error, processImage}
+}
+
+export default useProcessImage
