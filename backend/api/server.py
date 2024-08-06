7595c1cbff3a3567ad7d1160e510de890694562c
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-06 04:55:38 +0300

    Add data gathering ability

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, Response
 from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import base64
 import os
 
 from core.image_processor import ImageProcessor
 from core.yolo_model import YoloModel
+from firebase.utils import save_to_firebase
 
 class Server:
     def __init__(self) -> None:
@@ -12,6 +16,7 @@ class Server:
         self.yolo_model = YoloModel()
 
         self.__register_routes()
+        print('Server initialized...')
 
     def __register_routes(self) -> None:
         self.app.add_url_rule('/process_image', 'process_image', self.process_image, methods=['POST'])
@@ -25,22 +30,42 @@ class Server:
             if 'true_character' not in request.form:
                 return jsonify({'error': 'No true_character part'}), 400
             
+            image_64string: str = request.files['image_string']
+            image = Image.open(BytesIO(base64.b64decode(image_64string)))
             
             os.mkdir('images') if not os.path.isdir('images') else None
             image_path = 'images/uploaded_image.jpeg'  # Save uploaded image temporarily
-            image_file.save(image_path)
+            image.save(image_path)
             
-            self.image_processor.process_image(image_path, save=True, output_path='images/output.png')
-            detected_class = self.yolo_model.predict_class('images/output.png')
+            output_path = 'images/output.png'            
+            self.image_processor.process_image(image_path, save=True, output_path=output_path)
+            detected_character = self.yolo_model.predict_class(output_path)
             
-            return jsonify({'detected_class': detected_class})
+            processed_image = Image.open(output_path)
+            processed_image_output = BytesIO()
+            processed_image.save(processed_image_output, format='JPEG')
+            processed_image_64string = base64.b64encode(processed_image_output.getvalue()).decode('utf-8')
+            
+            time_stamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+            
+            data = {
+                'detected_character': detected_character,
+                'true_character': request.form['true_character'],
+                'og_image': image_64string,
+                'processed_image': processed_image_64string,
+                'server_time_stamp': time_stamp
+            }
+
+            save_to_firebase('images', time_stamp, data)
+            
+            for path in [image_path, output_path]:
+                if os.path.exists(path):
+                    os.remove(path)            
+
+            return jsonify({'result': data})
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
 
     def run(self):
         self.app.run(debug=True)
-
-if __name__ == '__main__':
-    server = Server()
-    server.run()
