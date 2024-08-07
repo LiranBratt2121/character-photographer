67dc46c3be1d6e30665477f212dfb30750a6fab8
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-07 17:35:09 +0300

    Update code to fit new classification model instead of detection model

--- backend/core/yolo_model.py
+++ backend/core/yolo_model.py
@@ -6,7 +6,7 @@ class YoloModel:
         self.model = YOLO(model_weights)
 
     def predict_class(self, image_path: str, conf=0.2) -> str:
-        result = self.model.predict(source=image_path, conf=conf, verbose=False, max_det=1)[0]
-        if result.boxes.shape[0] == 0: return 'Unknown'
-        class_name = result.names.get(result.boxes.cls[0].item(), 'Unknown')
+        result = self.model.predict(source=image_path, conf=conf, verbose=False)[0]
+        if not result.names: return 'Unknown'
+        class_name = result.names.get(result.probs.top1, 'Unknown')
         return class_name
