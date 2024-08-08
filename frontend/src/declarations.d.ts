bb3744c8e8875948afa8b740c5319c6542ac68fe
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-08 06:11:38 +0300

    Add declerations file for static imports of asset files

---
+++ frontend/src/declarations.d.ts
@@ -0,0 +1,28 @@
+// declarations.d.ts
+declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.gif' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.mp3' {
+    const value: string;
+    export default value;
+  }
+  
+  // Add other file types as needed
+  
